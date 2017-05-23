525143345f8ce8787399c3a8924c11419c2c7238
Chris Todorov <chris@freerunningtechnologies.com>
2017-05-23 18:48:55 +0100

    Parse tax table data into hash
    This change adds the ability to parse the tax tables into a `Hash` with
    the key being the first column - zipcode and the value a decimal
    representation of the second column - the tax rate.
    
    This change also moves the fixture files into separate folders, which ensures
    we can properly test tables w/ header rows and tables without header rows.

--- app/models/spree/tax_table.rb
+++ app/models/spree/tax_table.rb
@@ -2,7 +2,26 @@ require 'csv'
 
 module Spree
   class TaxTable
+    ZIPCODE_INDEX = 0
+    TAX_RATE_INDEX = 1
+
     class << self
+      def tax_table
+        @tax_table ||= parse_table_rows
+      end
+
+      private
+
+      def parse_table_rows
+        rows = load_tax_tables.map do |row|
+          [
+            row[ZIPCODE_INDEX].to_i,
+            BigDecimal.new(row[TAX_RATE_INDEX])
+          ]
+        end
+        Hash[rows]
+      end
+
       def load_tax_tables
         Dir["#{Spree::TaxTableConfiguration.tax_tables_path}/*.csv"].flat_map do |filename|
           Rails.logger.info "[spree_tax_table] Loading #{filename}"
--- spec/models/spree/tax_table_spec.rb
+++ spec/models/spree/tax_table_spec.rb
@@ -1,35 +1,36 @@
 require 'spec_helper'
 
 RSpec.describe Spree::TaxTable do
-  describe '.load_tax_tables' do
-    subject { described_class.load_tax_tables }
+  describe '.tax_table' do
+    subject { described_class.tax_table }
 
     before do
-      Spree::TaxTableConfiguration.tax_tables_path = "#{Rails.root}/../fixtures"
+      Spree::TaxTableConfiguration.tax_tables_path = tax_tables_path
       Spree::TaxTableConfiguration.skip_header_rows = skip_header_rows
     end
 
     context 'with `skip_header_rows` set to true' do
+      let(:tax_tables_path) { "#{Rails.root}/../fixtures/tax_tables_with_header" }
       let(:skip_header_rows) { true }
 
-      it 'skips the first row of each file' do
+      it 'skips the first row of the file' do
         expect(subject).to eq(
-          [
-            ['90210', '0.0875']
-          ]
+          {
+            90210 => BigDecimal.new("0.0875") # rubocop:disable NumericLiterals
+          }
         )
       end
     end
 
     context 'with `skip_header_rows` set to false' do
+      let(:tax_tables_path) { "#{Rails.root}/../fixtures/tax_tables_without_header" }
       let(:skip_header_rows) { false }
 
-      it 'does not skip the first row of each file' do
+      it 'does not skip the first row of the file' do
         expect(subject).to eq(
-          [
-            ['ZipCode', 'Rate'],
-            ['90210', '0.0875']
-          ]
+          {
+            90210 => BigDecimal.new("0.0875") # rubocop:disable NumericLiterals
+          }
         )
       end
     end
